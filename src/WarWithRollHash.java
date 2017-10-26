92b9fdd7610310e9b1f47aef154226e555bbe2c2
akkiel <akielbasa@sbcglobal.net>
2017-10-25 21:57:02 -0500

    update rollhash and testhash

--- src/WarWithRollHash.java
+++ src/WarWithRollHash.java
@@ -8,21 +8,89 @@
 //  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class WarWithRollHash {
 	// member fields and methods
+	private static int PRIME_BASE = 17;
+	private static int PRIME_MOD = 102564;
+
+	private String[] s;
+	private Hashtable<Integer, String> hTab;
+	private int k;
+	private ArrayList<String> t;
 
 	public WarWithRollHash(String[] s, int k) {
 		// implementation
+		this.s = s;
+		this.k = k;
+		this.t = new ArrayList<String>();
+		this.hTab = new Hashtable<Integer, String>();
+		for (String ss : s) {
+			this.hTab.put(hashStr(ss), ss);
+		}
+	}
+
+	private void verify(String verify) {
+		String sub;
+		boolean toAdd = true;
+		for (int i = 1; i < (2 * k) - 2; i++) {
+			sub = verify.substring(i, k + i);
+			int key = hashStr(sub);
+			int key2 = 0;
+			int power = 1;
+			for (int j = 0; j < k; j++) {
+				power = (power * PRIME_BASE) % PRIME_MOD;
+			}
+			for (int a = 0; a < 2 * k; a++) {
+				key2 = key2 * PRIME_BASE + verify.charAt(a);
+				key2 %= PRIME_MOD;
+				if (a >= k) {
+					key2 -= power * verify.charAt(a - k) % PRIME_MOD;
+					if (key2 < 0) {
+						key2 += PRIME_MOD;
+					}
+				}
+			}
+			toAdd = false;
+			// compute rolling hash
+			key = roll(verify, key, i);
+			if (hTab.contains(key)) {
+				toAdd = true;
+			}
+			if (!toAdd) {
+				return;
+			}
+		}
+		t.add(verify);
 	}
 
 	public ArrayList<String> compute2k() {
 		// implementation
-		return null;
+		for (int i = 0; i < s.length; i++) {
+			for (int j = i; j < s.length; j++) {
+				if (i == j) {
+					verify(s[i] + s[j]);
+				} else {
+					verify(s[i] + s[j]);
+					verify(s[j] + s[i]);
+				}
+			}
+		}
+		return t;
 	}
-	
+
 	public int hashStr(String str) {
-		
+		int ret = 0;
+		for (int i = 0; i < str.length(); i++) {
+			ret = ret * PRIME_BASE + str.charAt(i);
+			ret %= PRIME_MOD;
+		}
+		return ret;
+	}
+
+	private int roll(String str, int key, int i) {
+
 		return 0;
 	}
 }
--- test/testWarWithHash.java
+++ test/testWarWithHash.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
@@ -8,24 +9,75 @@ import org.junit.jupiter.api.Test;
 
 class testWarWithHash {
 	
-	
 	@Test
-	public void testCompute2k() {
+	public void testZero() {
+		String[] strSet = { "AA", "BB" };
+		// AAAA
+		// AABB x
+		// BBAA x
+		// BBBB
+		WarWithHash arr = new WarWithHash(strSet, 2);
+		ArrayList<String> computed = arr.compute2k();
+		assertEquals("Computed list size should be 2", 2, computed.size());
+	}
 
-		String[] strSet = {"AAA", "BBB", "CCC"};
-		WarWithHash hash = new WarWithHash(strSet, 3);
-		
-		ArrayList<String> computed = hash.compute2k();
-		
-		assertEquals("Computed list size should be strSet.size ^ 2 when there are no duplicates in strSet", strSet.length * strSet.length, computed.size());
-		
-		String[] newStrSet = {"AAA", "BBB", "CCC", "AAA", "CCC"};
+	@Test
+	public void testOne() {
+		String[] strSet = { "AA", "BB", "AB" };
+		// AAAA
+		// AABB
+		// BBAA x
+		// AAAB
+		// ABAA x
+		// BBBB
+		// BBAB x
+		// ABBB
+		// ABAB x
+		// only two possible outcomes
+		WarWithHash arr = new WarWithHash(strSet, 2);
+		ArrayList<String> computed = arr.compute2k();
+		assertEquals("Computed list size should be 5", 5, computed.size());
+	}
 
-		WarWithHash newHash = new WarWithHash(newStrSet, 3);
-		ArrayList<String> newComputed = newHash.compute2k();
-		
-		assertTrue("Both computed lists should be identical size the only difference is duplicated", computed.containsAll(newComputed));
-		
+	@Test
+	public void testTwo() {
+		String[] strSet = { "AA", "BB", "AB", "BA" };
+		// AAAA
+		// AABB
+		// BBAA
+		// AAAB
+		// ABAA
+		// AABA
+		// BAAA
+		// BBBB
+		// BBAB
+		// ABBB
+		// BBBA
+		// BABB
+		// ABAB
+		// ABBA
+		// BAAB
+		// BABA
+		// only two possible outcomes
+		WarWithHash arr = new WarWithHash(strSet, 2);
+		ArrayList<String> computed = arr.compute2k();
+		assertEquals("Computed list size should be 16", 16, computed.size());
+	}
+
+	@Test
+	public void testThree() {
+		String[] strSet = { "AAA", "ABA", "BAB" };
+		// AAAAAA
+		// AAAABA x
+		// ABAAAA x
+		// AAABAB x
+		// BABAAA x
+		// ABAABA x
+		// ABABAB
+		// BABABA
+		WarWithHash arr = new WarWithHash(strSet, 3);
+		ArrayList<String> computed = arr.compute2k();
+		assertEquals("Computer list size should be 3", 3, computed.size());
 	}
 	
 	
